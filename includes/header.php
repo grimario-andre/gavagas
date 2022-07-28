2f2dd9cf12b833e868df9f696fef6c68ff95d016
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-07-28 17:37:17 -0300

    Second Commit
    Construct header and footer with bootstrap

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body class="bg-dark text-light">
+
+    <!-- Inicio div container -->
+    <div class="container">
+        <!-- cabeçalho  -->
+        <div class="jumbotrom bg-danger">
+            <h1>GRISALHO DEV VAGAS</h1>
+            <p>Exemplo de CRUD de vagas</p>
+        </div>
+
+    
+
+
