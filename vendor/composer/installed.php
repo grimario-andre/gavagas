4a5cf82537c5dcf1e05a408379425c2cb49694dd
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-07-28 01:21:27 -0300

    Second Commit
    Config and Install composer.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9220b485ef7c24858748fab2ebcd67073da6ef8f',
+        'name' => 'gadev/vagas',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gadev/vagas' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9220b485ef7c24858748fab2ebcd67073da6ef8f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
