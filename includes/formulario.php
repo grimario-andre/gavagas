901d96f72c8c523c6ca4dd24862af8e56bf3f449
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-07-28 17:55:40 -0300

    Third Commit
    Start form

---
+++ includes/formulario.php
@@ -0,0 +1,34 @@
+<main>
+
+    <section>
+        <a href="index.php">
+            <button class="btn btn-success">
+                Voltar
+            </button>
+        </a>
+    </section>
+
+    <h2 class="mt-3">Cadastrar vaga</h2>
+
+    <!-- Inicio Form -->
+    <form action="" method="post">
+        <!-- Titulo -->
+        <div class="form-group mb-3">
+            <label for="titulo">Título</label>
+            <input type="text" placeholder="Ex: Desenvolvedor Jr. PHP" class="form-control" name="titulo">
+        </div>
+
+        <!-- Descrição -->
+        <div class="form-group mb-3">
+            <label for="descricao">Descrição</label>
+            <textarea class="form-control" placeholder="Ex: Desenvolvedor Jr. PHP" name="descricao" id="descricao" cols="30" rows="10"></textarea>
+        </div>
+
+        <!--  -->
+        <div class="form-group mb-3">
+            <label for="descricao">Descrição</label>
+            <input type="text" placeholder="Ex: Desenvolvedor Jr. PHP" class="form-control" name="descricao">
+        </div>
+
+    </form>
+</main>
