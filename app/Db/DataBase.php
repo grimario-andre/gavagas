331937aa645857aeb482f6da146070b1be5656e6
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-07-29 11:42:26 -0300

    Six Commit
    Create class DataBase

---
+++ app/Db/DataBase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Db;
+
+class DataBase
+{
+    /**
+     * Host de conexão com o banco de dados.
+     * @var string
+     */
+   const HOST = '127.0.0.1';
+   
+   /**
+    * Nome do banco de dados.
+    * @var string
+    */
+   const NAME = 'db_vagas';
+
+   /**
+    * Nome do usuario do banco de dados.
+    * @var string
+    */
+   const USER = 'root';
+
+   /**
+    * Senha do banco de dados.
+    *@var string
+    */
+   const PASS = '';
+
+}
