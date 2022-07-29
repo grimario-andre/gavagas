dd178c46f515ccd9012b95ae6faa99ec9fd38998
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-07-29 17:16:53 -0300

    Build class DataBase

--- app/Db/DataBase.php
+++ app/Db/DataBase.php
@@ -2,8 +2,12 @@
 
 namespace App\Db;
 
+use PDO, PDOException;
+
 class DataBase
 {
+
+    #region
     /**
      * Host de conexão com o banco de dados.
      * @var string
@@ -28,4 +32,92 @@ class DataBase
     */
    const PASS = '';
 
+
+   /**
+    * Nome da tabela a ser manipulado.
+    *
+    * @var string
+    */
+    private string $table;
+
+    /**
+     * Instância de conexao com o banco de *  dados.
+     *
+     * @var PDO
+     */
+    private PDO $connection;
+
+    #endregion
+
+    /**
+     * Instância da Classe.
+     *
+     * @param string $table
+     */
+    public function __construct($table = null)
+    {
+        $this->table = $table;
+        $this->setConnection();
+    }
+
+    /**
+     * Método responsável por criar uma conexão com a base
+     *
+     * @return void
+     */
+    private function setConnection()
+    {
+        try {
+            $this->connection = new PDO(
+                'mysql:host='.self::HOST.
+                ';dbname='.self::NAME,
+                self::USER,
+                self::PASS
+            );
+
+            $this->connection->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+        } catch (PDOException $exeception) {
+            echo 'ERROR: ' . $exeception->getMessage();
+        }
+    }
+
+
+    /**
+     * Método responsável por executar queries dentro da base
+     *
+     * @param string $query
+     * @param array  $params
+     * @return PDOStatement
+     */
+    public  function execute($query, $param = [])
+    {
+        
+    }
+
+    /**
+     * Métedo responsável por inserir dados no banco
+     *
+     * @param array $values [field => value] 
+     * @return int
+     */
+    public function insert($values)
+    {
+        //DADOS DA QUERY
+        $fields = array_keys($values);
+        $binds  = array_pad([], count($values), '?');
+
+        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';  
+
+        echo '<pre>';
+        print_r($query); 
+        echo '</pre>'; 
+        exit;
+        
+    }
+
 }
+
