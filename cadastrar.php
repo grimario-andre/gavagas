2036e4747a41a0243467dc41ea9b767f2b55704e
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-07-31 12:14:42 -0300

    Seven Commit
    Finish in cadastrar and listagem.

--- cadastrar.php
+++ cadastrar.php
@@ -1,20 +1,25 @@
-<?php 
+<?php
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Entity\Vaga;
 
 //Validar Form Post cadastrar.
 
 if (isset($_POST['titulo'], $_POST['descricao'], $_POST['ativo'])) {
-    $obVaga = new Vaga();
-
+    $obVaga = new Vaga;
     $obVaga->titulo    = $_POST['titulo'];
     $obVaga->descricao = $_POST['descricao'];
     $obVaga->ativo     = $_POST['ativo'];
     $obVaga->cadastrar();
+
+    //CABECALHO
+    header('location: index.php?status=success');
+    exit;
 }
 
-require_once __DIR__.'/includes/header.php';
-require_once __DIR__.'/includes/formulario.php';
-require_once __DIR__.'/includes/footer.php';
+
+
+require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/includes/formulario.php';
+require_once __DIR__ . '/includes/footer.php';
--- includes/listagem.php
+++ includes/listagem.php
@@ -1,5 +1,32 @@
-<main>
+<?php
+    $resultados = '';
 
+    foreach ($vagas as $vaga) {
+     
+        $resultados .= '
+            <tr>
+                <td>'. $vaga->id .'</td>
+                <td>'. $vaga->titulo .'</td>
+                <td>'. $vaga->descricao .'</td>
+                <td>'. ($vaga->ativo == 's' ? ' Ativo ' : ' Inativo ') .'</td>
+                <td>'. date('d/m/Y a\s H:i:s', strtotime($vaga->data) ) .'</td>
+                <td>
+                    <a href="editar.php?id='. $vaga->id .' ">
+                        <button type="button" class="btn btn-primary">Editar</button>
+                    </a>
+
+                    <a href="excluir.php?id='. $vaga->id .' ">
+                        <button type="button" class="btn btn-danger">Excluir</button>
+                    </a>
+                </td>    
+            </tr>
+        ';
+    }
+
+?>
+
+<main>
+    <!-- Sessao Cadastrar -->
     <section>
         <a href="cadastrar.php">
             <button class="btn btn-success">
@@ -8,4 +35,24 @@
         </a>
     </section>
 
+    <!-- Tabela Vagas -->
+    <section>
+        <!-- HEAD -->
+        <table class="table bg-light mt-3">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>TITULO</th>
+                    <th>DESCRICAO</th>
+                    <th>STATUS</th>
+                    <th>DATA</th>
+                    <th>ACOES</th>
+                </tr>
+            </thead>
+            <!-- BODY -->
+            <tbody>
+                <?=$resultados?>
+            </tbody>
+        </table>
+    </section>
 </main>
