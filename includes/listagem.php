823b9794263b77f59b77c80fc6c91f594a028e62
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2022-08-12 23:39:08 -0300

    Twelve Commit
    Filter and Search finished.

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,44 +1,44 @@
 <?php
-    //MENSAGEM DE CABECALHO
-    $mensagem = '';
-    if (isset($_GET['status'])) {
-        switch ($_GET['status']) {
-            case 'error':
-                $mensagem = '<div class="alert alert-danger">Acao nao Executada!</div>';
-                break;
-            
-            default:
-                $mensagem = '<div class="alert alert-success">Acao Executada!</div>';
-                break;
-        }
+//MENSAGEM DE CABECALHO
+$mensagem = '';
+if (isset($_GET['status'])) {
+    switch ($_GET['status']) {
+        case 'error':
+            $mensagem = '<div class="alert alert-danger">Acao nao Executada!</div>';
+            break;
+
+        default:
+            $mensagem = '<div class="alert alert-success">Acao Executada!</div>';
+            break;
     }
+}
 
-    //EXIBIR LISTA VAGAS
-    $resultados = '';
+//EXIBIR LISTA VAGAS
+$resultados = '';
 
-    foreach ($vagas as $vaga) {
-     
-        $resultados .= '
+foreach ($vagas as $vaga) {
+
+    $resultados .= '
             <tr>
-                <td>'. $vaga->id .'</td>
-                <td>'. $vaga->titulo .'</td>
-                <td>'. $vaga->descricao .'</td>
-                <td>'. ($vaga->ativo == 's' ? ' Ativo ' : ' Inativo ') .'</td>
-                <td>'. date('d/m/Y a\s H:i:s', strtotime($vaga->data) ) .'</td>
+                <td>' . $vaga->id . '</td>
+                <td>' . $vaga->titulo . '</td>
+                <td>' . $vaga->descricao . '</td>
+                <td>' . ($vaga->ativo == 's' ? ' Ativo ' : ' Inativo ') . '</td>
+                <td>' . date('d/m/Y a\s H:i:s', strtotime($vaga->data)) . '</td>
                 <td>
-                    <a href="editar.php?id='. $vaga->id .' ">
+                    <a href="editar.php?id=' . $vaga->id . ' ">
                         <button type="button" class="btn btn-primary">Editar</button>
                     </a>
 
-                    <a href="excluir.php?id='. $vaga->id .' ">
+                    <a href="excluir.php?id=' . $vaga->id . ' ">
                         <button type="button" class="btn btn-danger">Excluir</button>
                     </a>
                 </td>    
             </tr>
         ';
-    }
+}
 
-    $resultados = strlen($resultados) ? $resultados : '
+$resultados = strlen($resultados) ? $resultados : '
     <tr> 
         <td colspan="6" class="text-center"><p><strong>Nenhuma Vaga Encontrada</strong></p></td>
     </tr>    ';
@@ -47,7 +47,7 @@
 
 <main>
     <!-- Exibir msn Cabecalho -->
-    <?=$mensagem?>
+    <?= $mensagem ?>
 
     <!-- Sessao Cadastrar -->
     <section>
@@ -56,6 +56,35 @@
                 Nova Vaga
             </button>
         </a>
+    </section>
+
+    <!-- Busca -->
+    <section class="">
+        <form method="get" class="">
+            <!-- GRID -->
+            <div class="row my-4">
+                <div class="col">
+                    <label for="busca">
+                        Buscar por Titulo
+                    </label>
+                    <input class="form-control" type="text" name="busca" id="busca" value="<?= $busca ?>">
+                </div>
+
+                <! -- Buscar Opcoes -->
+                <div class="col">
+                    <label>Status</label>
+                    <select name="status" id="status" class="form-control">
+                        <option value="">Ativa/Inativa</option>
+                        <option value="s" <?=$filtroStatus == 's' ? 'selected' : '' ?> >Ativa</option>
+                        <option value="n" <?=$filtroStatus == 'n' ? 'selected' : '' ?> >Inativa</option>
+                    </select>
+                </div>
+
+                <div class="col d-flex align-items-end">
+                    <button class="btn btn-primary" type="submit">Filtrar</button>
+                </div>
+            </div>
+        </form>
     </section>
 
     <!-- Tabela Vagas -->
@@ -74,7 +103,7 @@
             </thead>
             <!-- BODY -->
             <tbody>
-                <?=$resultados?>
+                <?= $resultados ?>
             </tbody>
         </table>
     </section>
--- index.php
+++ index.php
@@ -4,7 +4,26 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use App\Entity\Vaga;
 
-$vagas = Vaga::getVagas();
+//BUSCA
+$busca = filter_input(INPUT_GET, 'busca', FILTER_SANITIZE_STRING);
+
+//FILTRO STATUS
+$filtroStatus = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$filtroStatus = in_array($filtroStatus,['s','n']) ? $filtroStatus : '';
+
+//CODICOES SQL
+$condicoes = [
+    strlen($busca) ? 'titulo LIKE "%'. str_replace('','%', $busca) .'%"' : null,
+    strlen($filtroStatus) ? 'ativo = "'. $filtroStatus .'"' : null
+];
+
+//REMOVE POSICOES VAZIAS
+$condicoes = array_filter($condicoes);
+
+//CLAUSULA WHERE
+$where = implode(' AND ', $condicoes);
+
+$vagas = Vaga::getVagas($where);
 
 require_once __DIR__.'/includes/header.php';
 require_once __DIR__.'/includes/listagem.php';
